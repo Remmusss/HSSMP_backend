333125d6f8d49afdfe222331d3adf59c6ad148b9
Remmusss <tdmq207@gmail.com>
2025-05-13 00:31:33 +0700

    update bla

--- main.py
+++ main.py
@@ -9,29 +9,42 @@ from fastapi import (
 )
 from fastapi.responses import RedirectResponse
 
-# from src.routers.auth import auth_router
+from fastapi.middleware.cors import CORSMiddleware
+
+
+from src.routers.auth import auth_router
 from src.routers.employees import employees_router
 from src.routers.payroll import payroll_router
 from src.routers.departments import departments_router
 from src.routers.positions import positions_router
 from src.routers.reports import reports_router
-from src.routers.auth import auth_router
 
-# uvicorn main:app --reload
 app = FastAPI(
     title="ZENHRM SYSTEM MANAGEMENT",
     description="App quản lý nhân sự và bảng lương",
     version="0.9.9",
 )
 
+origins = [
+    "*"
+]
 
-# app.include_router(auth_router, prefix="/auth")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# uvicorn main:app --reload
+
+app.include_router(auth_router, prefix="/auth")
 app.include_router(employees_router, prefix="/employees")
 app.include_router(payroll_router, prefix="/payroll")
 app.include_router(departments_router, prefix="/departments")
 app.include_router(positions_router, prefix="/positions")
 app.include_router(reports_router, prefix="/reports")
-app.include_router(auth_router, prefix="/auth")
 
 
 @app.get("/")
