fe2cdaa2795fac589ff44ba1f38329173954c52a
Remmusss <tdmq207@gmail.com>
2025-05-16 21:24:17 +0700

    fix search employee

--- src/utils/employees.py
+++ src/utils/employees.py
@@ -11,23 +11,21 @@ from ..schemas.human import (
     Department as HmDepartment,
     Employee as HmEmployee,
     Dividend as HmDividend,
-    Position as HmPosition
+    Position as HmPosition,
 )
 from ..schemas.payroll import (
     Department as PrDepartment,
     Employee as PrEmployee,
     Salary as PrSalary,
-    Attendance as PrAttendance
+    Attendance as PrAttendance,
 )
 
-from ..models.human import (
-    EmployeeCreate,
-    EmployeeUpdate
-)
+from ..models.human import EmployeeCreate, EmployeeUpdate
 
 
-
-def get_employees(session: Session, page: int = 1, per_page: int = 10) -> List[HmEmployee]:
+def get_employees(
+    session: Session, page: int = 1, per_page: int = 10
+) -> List[HmEmployee]:
     query = session.query(HmEmployee).options(
         joinedload(HmEmployee.department), joinedload(HmEmployee.position)
     )
@@ -39,32 +37,29 @@ def get_employees(session: Session, page: int = 1, per_page: int = 10) -> List[H
     return results
 
 
-
 def search_employees_logic(
-    session: Session,
-    search_query: str = None,
-    page: int = 1,
-    per_page: int = 10
+    session: Session, search_query: str = None, page: int = 1, per_page: int = 10
 ):
-    query = session.query(HmEmployee).options(
-        joinedload(HmEmployee.department),
-        joinedload(HmEmployee.position)
-    )
+    query = (
+        session.query(HmEmployee)
+        .join(HmDepartment, HmEmployee.DepartmentID == HmDepartment.DepartmentID)
+        .join(HmPosition, HmEmployee.PositionID == HmPosition.PositionID)
+    ).options(joinedload(HmEmployee.department), joinedload(HmEmployee.position))
 
     if search_query:
         try:
             numeric_query = int(search_query)
             query = query.filter(
-                (HmEmployee.EmployeeID == numeric_query) |
-                (HmEmployee.FullName.ilike(f"%{search_query}%")) |
-                (HmEmployee.department.DepartmentName.ilike(f"%{search_query}%")) |
-                (HmEmployee.position.PositionName.ilike(f"%{search_query}%"))
+                (HmEmployee.EmployeeID == numeric_query)
+                | (HmEmployee.FullName.ilike(f"%{search_query}%"))
+                | (HmDepartment.DepartmentName.ilike(f"%{search_query}%"))
+                | (HmPosition.PositionName.ilike(f"%{search_query}%"))
             )
         except ValueError:
             query = query.filter(
-                (HmEmployee.FullName.ilike(f"%{search_query}%")) |
-                (HmEmployee.department.DepartmentName.ilike(f"%{search_query}%")) |
-                (HmEmployee.position.PositionName.ilike(f"%{search_query}%"))
+                (HmEmployee.FullName.ilike(f"%{search_query}%"))
+                | (HmDepartment.DepartmentName.ilike(f"%{search_query}%"))
+                | (HmPosition.PositionName.ilike(f"%{search_query}%"))
             )
 
     offset = (page - 1) * per_page
@@ -73,11 +68,20 @@ def search_employees_logic(
     return results
 
 
-
-def add_and_sync_employee(session_human: Session, session_payroll: Session, employee: EmployeeCreate):
+def add_and_sync_employee(
+    session_human: Session, session_payroll: Session, employee: EmployeeCreate
+):
     if employee.EmployeeID is not None:
-        exists_human = session_human.query(HmEmployee).filter_by(EmployeeID=employee.EmployeeID).first()
-        exists_payroll = session_payroll.query(PrEmployee).filter_by(EmployeeID=employee.EmployeeID).first()
+        exists_human = (
+            session_human.query(HmEmployee)
+            .filter_by(EmployeeID=employee.EmployeeID)
+            .first()
+        )
+        exists_payroll = (
+            session_payroll.query(PrEmployee)
+            .filter_by(EmployeeID=employee.EmployeeID)
+            .first()
+        )
 
         if exists_human or exists_payroll:
             source = []
@@ -85,21 +89,44 @@ def add_and_sync_employee(session_human: Session, session_payroll: Session, empl
                 source.append("HUMAN_2025")
             if exists_payroll:
                 source.append("payroll")
-            raise HTTPException(status_code=400, detail=f"EmployeeID đã tồn tại trong: {', '.join(source)}")
+            raise HTTPException(
+                status_code=400,
+                detail=f"EmployeeID đã tồn tại trong: {', '.join(source)}",
+            )
     else:
-        max_id_human = session_human.query(HmEmployee.EmployeeID).order_by(HmEmployee.EmployeeID.desc()).first()
-        max_id_payroll = session_payroll.query(PrEmployee.EmployeeID).order_by(PrEmployee.EmployeeID.desc()).first()
+        max_id_human = (
+            session_human.query(HmEmployee.EmployeeID)
+            .order_by(HmEmployee.EmployeeID.desc())
+            .first()
+        )
+        max_id_payroll = (
+            session_payroll.query(PrEmployee.EmployeeID)
+            .order_by(PrEmployee.EmployeeID.desc())
+            .first()
+        )
 
         max_id = max(
             max_id_human[0] if max_id_human else 0,
-            max_id_payroll[0] if max_id_payroll else 0
+            max_id_payroll[0] if max_id_payroll else 0,
         )
         employee.EmployeeID = max_id + 1
 
-    if session_human.query(HmEmployee).filter_by(EmployeeID=employee.EmployeeID).first():
-        raise HTTPException(status_code=400, detail="EmployeeID đã tồn tại trong HUMAN_2025")
-    if session_payroll.query(PrEmployee).filter_by(EmployeeID=employee.EmployeeID).first():
-        raise HTTPException(status_code=400, detail="EmployeeID đã tồn tại trong payroll")
+    if (
+        session_human.query(HmEmployee)
+        .filter_by(EmployeeID=employee.EmployeeID)
+        .first()
+    ):
+        raise HTTPException(
+            status_code=400, detail="EmployeeID đã tồn tại trong HUMAN_2025"
+        )
+    if (
+        session_payroll.query(PrEmployee)
+        .filter_by(EmployeeID=employee.EmployeeID)
+        .first()
+    ):
+        raise HTTPException(
+            status_code=400, detail="EmployeeID đã tồn tại trong payroll"
+        )
 
     try:
         new_human_emp = HmEmployee(
@@ -112,7 +139,7 @@ def add_and_sync_employee(session_human: Session, session_payroll: Session, empl
             HireDate=employee.HireDate,
             DepartmentID=employee.DepartmentID,
             PositionID=employee.PositionID,
-            Status=employee.Status
+            Status=employee.Status,
         )
         session_human.add(new_human_emp)
         session_human.commit()
@@ -122,22 +149,34 @@ def add_and_sync_employee(session_human: Session, session_payroll: Session, empl
             FullName=employee.FullName,
             DepartmentID=employee.DepartmentID,
             PositionID=employee.PositionID,
-            Status=employee.Status
+            Status=employee.Status,
         )
         session_payroll.add(new_payroll_emp)
         session_payroll.commit()
 
-        return {"message": "Nhân viên đã được thêm và đồng bộ thành công.", "EmployeeID": employee.EmployeeID}
+        return {
+            "message": "Nhân viên đã được thêm và đồng bộ thành công.",
+            "EmployeeID": employee.EmployeeID,
+        }
 
     except Exception as e:
         session_human.rollback()
         session_payroll.rollback()
-        raise HTTPException(status_code=500, detail=f"Lỗi khi thêm nhân viên: {str(e)}")    
-    
+        raise HTTPException(status_code=500, detail=f"Lỗi khi thêm nhân viên: {str(e)}")
 
-def update_and_sync_employee(session_human : Session, session_payroll : Session, employee_id : int, update_data : EmployeeUpdate):
-    human_emp = session_human.query(HmEmployee).filter_by(EmployeeID=employee_id).first()
-    payroll_emp = session_payroll.query(PrEmployee).filter_by(EmployeeID=employee_id).first()
+
+def update_and_sync_employee(
+    session_human: Session,
+    session_payroll: Session,
+    employee_id: int,
+    update_data: EmployeeUpdate,
+):
+    human_emp = (
+        session_human.query(HmEmployee).filter_by(EmployeeID=employee_id).first()
+    )
+    payroll_emp = (
+        session_payroll.query(PrEmployee).filter_by(EmployeeID=employee_id).first()
+    )
 
     if not human_emp or not payroll_emp:
         missing = []
@@ -145,7 +184,10 @@ def update_and_sync_employee(session_human : Session, session_payroll : Session,
             missing.append("HUMAN_2025")
         if not payroll_emp:
             missing.append("payroll")
-        raise HTTPException(status_code=404, detail=f"Nhân viên không tồn tại trong: {', '.join(missing)}")
+        raise HTTPException(
+            status_code=404,
+            detail=f"Nhân viên không tồn tại trong: {', '.join(missing)}",
+        )
 
     try:
         for field, value in update_data.model_dump(exclude_unset=True).items():
@@ -153,45 +195,65 @@ def update_and_sync_employee(session_human : Session, session_payroll : Session,
         session_human.commit()
     except Exception as e:
         session_human.rollback()
-        raise HTTPException(status_code=500, detail=f"Lỗi cập nhật trong HUMAN_2025: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"Lỗi cập nhật trong HUMAN_2025: {str(e)}"
+        )
 
     try:
         #  chỉ cập nhật 3 trường là DepartmentID, PositionID, Status
-        for field in ['DepartmentID', 'PositionID', 'Status']:
+        for field in ["DepartmentID", "PositionID", "Status"]:
             if field in update_data.model_dump(exclude_unset=True):
                 setattr(payroll_emp, field, getattr(human_emp, field))
         session_payroll.commit()
     except Exception as e:
         session_payroll.rollback()
-        session_human.rollback()  
-        raise HTTPException(status_code=500, detail=f"Lỗi cập nhật trong payroll: {str(e)}")
+        session_human.rollback()
+        raise HTTPException(
+            status_code=500, detail=f"Lỗi cập nhật trong payroll: {str(e)}"
+        )
 
     return {"message": "Cập nhật và đồng bộ nhân viên thành công."}
 
 
-def delete_employee_logic(session_human : Session, session_payroll: Session, employee_id: int):
-    human_emp = session_human.query(HmEmployee).filter_by(EmployeeID=employee_id).first()
-    payroll_emp = session_payroll.query(PrEmployee).filter_by(EmployeeID=employee_id).first()
+def delete_employee_logic(
+    session_human: Session, session_payroll: Session, employee_id: int
+):
+    human_emp = (
+        session_human.query(HmEmployee).filter_by(EmployeeID=employee_id).first()
+    )
+    payroll_emp = (
+        session_payroll.query(PrEmployee).filter_by(EmployeeID=employee_id).first()
+    )
 
     if not human_emp and not payroll_emp:
-        raise HTTPException(status_code=404, detail="Nhân viên không tồn tại trong cả hai hệ thống.")
+        raise HTTPException(
+            status_code=404, detail="Nhân viên không tồn tại trong cả hai hệ thống."
+        )
 
     if human_emp:
-        dividend_exists = session_human.query(HmDividend).filter_by(EmployeeID=employee_id).first()
+        dividend_exists = (
+            session_human.query(HmDividend).filter_by(EmployeeID=employee_id).first()
+        )
         if dividend_exists:
             raise HTTPException(
                 status_code=400,
-                detail="Không thể xóa. Nhân viên có dữ liệu cổ tức trong HUMAN_2025."
+                detail="Không thể xóa. Nhân viên có dữ liệu cổ tức trong HUMAN_2025.",
             )
 
     if payroll_emp:
-        salary_exists = session_payroll.query(PrSalary).filter_by(EmployeeID=employee_id).first()
-        attendance_exists = session_payroll.query(PrAttendance).filter_by(EmployeeID=employee_id).first()
+        salary_exists = (
+            session_payroll.query(PrSalary).filter_by(EmployeeID=employee_id).first()
+        )
+        attendance_exists = (
+            session_payroll.query(PrAttendance)
+            .filter_by(EmployeeID=employee_id)
+            .first()
+        )
 
         if salary_exists or attendance_exists:
             raise HTTPException(
                 status_code=400,
-                detail="Không thể xóa. Nhân viên có dữ liệu lương hoặc chấm công trong payroll."
+                detail="Không thể xóa. Nhân viên có dữ liệu lương hoặc chấm công trong payroll.",
             )
 
     try:
@@ -209,17 +271,19 @@ def delete_employee_logic(session_human : Session, session_payroll: Session, emp
         session_human.rollback()
         session_payroll.rollback()
         raise HTTPException(status_code=500, detail=f"Lỗi khi xóa nhân viên: {str(e)}")
-    
+
 
 def view_employee_details_logic(session: Session, employee_id: int):
     # if not employee_id:
     #     raise HTTPException(status_code=400, detail="EmployeeID trống")
 
-    employee = session.query(HmEmployee).options(
-        joinedload(HmEmployee.department),
-        joinedload(HmEmployee.position)
-    ).filter(HmEmployee.EmployeeID == employee_id).first()
-    
+    employee = (
+        session.query(HmEmployee)
+        .options(joinedload(HmEmployee.department), joinedload(HmEmployee.position))
+        .filter(HmEmployee.EmployeeID == employee_id)
+        .first()
+    )
+
     # if not employee:
     #     raise HTTPException(status_code=404, detail="EmployeeID không tồn tại")
 
@@ -227,7 +291,7 @@ def view_employee_details_logic(session: Session, employee_id: int):
         return None
 
     return {
-        "EmployeeID": employee.EmployeeID ,
+        "EmployeeID": employee.EmployeeID,
         "FullName": employee.FullName,
         "DateOfBirth": employee.DateOfBirth,
         "Gender": employee.Gender,
@@ -235,8 +299,10 @@ def view_employee_details_logic(session: Session, employee_id: int):
         "Email": employee.Email,
         "HireDate": employee.HireDate,
         "DepartmentID": employee.DepartmentID,
-        "DepartmentName": employee.department.DepartmentName if employee.department else None,
+        "DepartmentName": employee.department.DepartmentName
+        if employee.department
+        else None,
         "PositionID": employee.PositionID,
         "PositionName": employee.position.PositionName if employee.position else None,
-        "Status": employee.Status
+        "Status": employee.Status,
     }
--- src/utils/payroll.py
+++ src/utils/payroll.py
@@ -13,29 +13,26 @@ from ..schemas.payroll import (
     Salary as PrSalary,
     Attendance as PrAttendance,
 )
-from ..models.payroll import ( 
-    SalaryReportByDepartment, 
-    AttendanceWarning, 
-    SalaryDifferenceWarning, 
-    PayrollUpdate
+from ..models.payroll import (
+    SalaryReportByDepartment,
+    AttendanceWarning,
+    SalaryDifferenceWarning,
+    PayrollUpdate,
 )
 
 from datetime import date, timedelta
 from typing import Optional, List
 
 
-
-def get_payroll(
-        session: Session, 
-        page: int = 1, 
-        per_page: int = 10
-        ):
+def get_payroll(session: Session, page: int = 1, per_page: int = 10):
     query = session.query(PrSalary).join(
         PrEmployee, PrSalary.EmployeeID == PrEmployee.EmployeeID
     )
 
     offset = (page - 1) * per_page
-    results = query.order_by(PrSalary.SalaryMonth.desc()).offset(offset).limit(per_page).all()
+    results = (
+        query.order_by(PrSalary.SalaryMonth.desc()).offset(offset).limit(per_page).all()
+    )
 
     return [
         {
@@ -47,18 +44,15 @@ def get_payroll(
             "Bonus": salary.Bonus,
             "Deductions": salary.Deductions,
             "NetSalary": salary.NetSalary,
-            "Status": salary.employee.Status
+            "Status": salary.employee.Status,
         }
         for salary in results
     ]
 
 
 def search_payroll_logic(
-        session: Session, 
-        search_query: str = None,
-        page: int = 1,
-        per_page: int = 10
-        ):
+    session: Session, search_query: str = None, page: int = 1, per_page: int = 10
+):
     query = session.query(PrSalary).join(
         PrEmployee, PrSalary.EmployeeID == PrEmployee.EmployeeID
     )
@@ -67,17 +61,17 @@ def search_payroll_logic(
         try:
             numeric_query = int(search_query)
             query = query.filter(
-                (PrEmployee.EmployeeID == numeric_query) |
-                (PrSalary.SalaryID == numeric_query) |
-                (PrEmployee.FullName.ilike(f"%{search_query}%"))
+                (PrEmployee.EmployeeID == numeric_query)
+                | (PrSalary.SalaryID == numeric_query)
+                | (PrEmployee.FullName.ilike(f"%{search_query}%"))
             )
         except ValueError:
-            query = query.filter(
-                (PrEmployee.FullName.ilike(f"%{search_query}%"))
-            )
+            query = query.filter((PrEmployee.FullName.ilike(f"%{search_query}%")))
 
     offset = (page - 1) * per_page
-    results = query.order_by(PrSalary.SalaryMonth.desc()).offset(offset).limit(per_page).all()
+    results = (
+        query.order_by(PrSalary.SalaryMonth.desc()).offset(offset).limit(per_page).all()
+    )
 
     return [
         {
@@ -89,72 +83,76 @@ def search_payroll_logic(
             "Bonus": salary.Bonus,
             "Deductions": salary.Deductions,
             "NetSalary": salary.NetSalary,
-            "Status": salary.employee.Status
+            "Status": salary.employee.Status,
         }
         for salary in results
     ]
 
-def update_payroll(session: Session, payroll_id: int, update_data : PayrollUpdate):
+
+def update_payroll(session: Session, payroll_id: int, update_data: PayrollUpdate):
     payroll_emp = session.query(PrSalary).filter_by(SalaryID=payroll_id).first()
 
     if not payroll_emp:
-        raise HTTPException(status_code=404, detail=f"Salary ID không tồn tại trong: payroll")
-    
+        raise HTTPException(
+            status_code=404, detail=f"Salary ID không tồn tại trong: payroll"
+        )
 
     # chỉ cập nhật 2 trường là Bonus và Deductions từ đâu vào
     try:
         for field, value in update_data.model_dump(exclude_unset=True).items():
             setattr(payroll_emp, field, value)
-        payroll_emp.NetSalary = payroll_emp.BaseSalary + payroll_emp.Bonus - payroll_emp.Deductions
+        payroll_emp.NetSalary = (
+            payroll_emp.BaseSalary + payroll_emp.Bonus - payroll_emp.Deductions
+        )
         session.commit()
     except Exception as e:
         session.rollback()
-        raise HTTPException(status_code=500, detail=f"Lỗi khi cập nhật trong payroll: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"Lỗi khi cập nhật trong payroll: {str(e)}"
+        )
 
     return {"message": f"Cập nhật bảng lương {payroll_id} thành công."}
 
 
 def get_attendance_records(
-        session: Session, 
-        page: int = 1, 
-        per_page: int = 10,
-        search_date: Optional[date] = None, 
-        employee_id: Optional[int] = None
-        )-> List[PrAttendance]:
+    session: Session,
+    page: int = 1,
+    per_page: int = 10,
+    search_date: Optional[date] = None,
+    employee_id: Optional[int] = None,
+) -> List[PrAttendance]:
     query = session.query(PrAttendance).options(
         joinedload(PrAttendance.employee).joinedload(PrEmployee.department),
-        joinedload(PrAttendance.employee).joinedload(PrEmployee.position)
+        joinedload(PrAttendance.employee).joinedload(PrEmployee.position),
     )
 
     if search_date:
         query = query.filter(
-            extract('year', PrAttendance.AttendanceMonth) == search_date.year,
-            extract('month', PrAttendance.AttendanceMonth) == search_date.month
+            extract("year", PrAttendance.AttendanceMonth) == search_date.year,
+            extract("month", PrAttendance.AttendanceMonth) == search_date.month,
         )
 
     if employee_id:
         query = query.filter(PrAttendance.EmployeeID == employee_id)
 
     offset = (page - 1) * per_page
-    results = query.order_by(PrAttendance.AttendanceMonth.desc()).offset(offset).limit(per_page).all()
+    results = (
+        query.order_by(PrAttendance.AttendanceMonth.desc())
+        .offset(offset)
+        .limit(per_page)
+        .all()
+    )
 
     return results
 
-def get_personal_attendance(
-        session: Session,
-        employee_id: int
-        )-> List[PrAttendance]:
 
+def get_personal_attendance(session: Session, employee_id: int) -> List[PrAttendance]:
     query = session.query(PrAttendance).filter(PrAttendance.EmployeeID == employee_id)
 
     return query.all()
 
 
-def get_personal_payroll(
-        session: Session,
-        employee_id: int
-        )-> List[PrSalary]:
-    
+def get_personal_payroll(session: Session, employee_id: int) -> List[PrSalary]:
     query = session.query(PrSalary).filter(PrSalary.EmployeeID == employee_id)
 
     return query.all()
